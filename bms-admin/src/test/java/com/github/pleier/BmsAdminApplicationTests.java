a52e684aa16071b635cb9d71084926105dad5726
plei <pleiyang@outlook.com>
2018-03-19 17:56:09 +0800

    init

---
+++ bms-admin/src/test/java/com/github/pleier/BmsAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pleier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BmsAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
