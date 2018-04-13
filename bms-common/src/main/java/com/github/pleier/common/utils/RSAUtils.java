453dc4c5a2ed3d7aeeead372a1595ae043371cb9
plei <pleiyang@outlook.com>
2018-04-13 15:32:50 +0800

    Initial commit

--- bms-common/src/main/java/com/github/pleier/common/utils/RSAUtils.java
+++ bms-common/src/main/java/com/github/pleier/common/utils/RSAUtils.java
@@ -1,6 +1,6 @@
 package com.github.pleier.common.utils;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
