347c7370a7b87411cca0cddab1122aa958177749
plei <pleiyang@outlook.com>
2018-04-16 10:13:53 +0800

    Initial commit

---
+++ bms-api/src/main/java/com/github/pleier/bmsapi/BmsApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.pleier.bmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsApiApplication.class, args);
+	}
+}
---
+++ bms-api/src/test/java/com/github/pleier/bmsapi/BmsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pleier.bmsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BmsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
